c70da2626fcedbc39492ef8e7ceb3427dc150e24
Michael Coloma Calva <neomichael87@gmail.com>
2024-07-24 18:49:17 +0200

    Initialize project using Create React App with typescript and his configuration initial together with - webpack (with module css) - tsconfig - prettier - lint - jest - packge.json with all the possibles dependencies and scripts

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === "development";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      publicPath: '/',
+      path: path.resolve(__dirname, "dist"),
+      filename: isDevelopment ? "[name].js" : "[name].[contenthash].js",
+      clean: true // Clean directory output per build
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/marvel/adapters/primary/ui/components'),
+        '@assets': path.resolve(__dirname, 'src/marvel/adapters/primary/ui/assets'),
+        'src': path.resolve(__dirname, 'src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.module\.css$/,
+          use: [
+            isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  mode: "local",
+                  localIdentName: isDevelopment
+                    ? "[path][name]__[local]--[hash:base64:5]"
+                    : "[hash:base64:5]",
+                },
+                importLoaders: 1,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: /\.module\.css$/,
+          use: [
+            isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i,
+          type: "asset/resource",
+        }
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: false, // Delete comments
+            },
+          },
+          extractComments: false,
+        }),
+        new CssMinimizerPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? "[name].css" : "[name].[contenthash].css",
+        chunkFilename: isDevelopment ? "[id].css" : "[id].[contenthash].css",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    performance: {
+      maxAssetSize: 512000,
+      maxEntrypointSize: 512000,
+    },
+  };
+};
