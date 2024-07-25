53d4618ef3d85491c138b53ca9db32881c617f65
Michael Coloma Calva <neomichael87@gmail.com>
2024-07-25 12:14:04 +0200

    Add constants

---
+++ src/marvel/adapters/primary/types/constants.ts
@@ -0,0 +1,2 @@
+export const LIMIT_FETCH_CHARACTERS = 50;
+export const REPLACE_SIZE_IMAGE = "REPLACE_SIZE_IMAGE"; //Symbol('replace_size_image')
---
+++ src/marvel/adapters/primary/types/enums.ts
@@ -0,0 +1,26 @@
+export enum PORTRAIT_SIZE {
+  SMALL_50X75 = "portrait_small", // 50x75px
+  MEDIUM_100X150 = "portrait_medium", // 100x150px
+  XLARGE_150X225 = "portrait_xlarge", // 150x225px
+  FANTASTIC_168X252 = "portrait_fantastic", // 168x252px
+  UNCANNY_300X450 = "portrait_uncanny", // 300x450px
+  INCREDIBLE_216X324 = "portrait_incredible", // 216x324px
+}
+
+export enum STANDARD_SIZE {
+  SMALL_65X45 = "standard_small", // 65x45px
+  MEDIUM_100X100 = "standard_medium", // 100x100px
+  LARGE_140X140 = "standard_large", // 140x140px
+  XLARGE_200X200 = "standard_xlarge", // 200x200px
+  FANTASTIC_250X250 = "standard_fantastic", // 250x250px
+  AMAZING_180X180 = "standard_amazing", // 180x180px
+}
+
+export enum LANDSCAPE_SIZE {
+  SMALL_120X90 = "landscape_small", // 120x90px
+  MEDIUM_175X130 = "landscape_medium", // 175x130px
+  LARGE_190X140 = "landscape_large", // 190x140px
+  XLARGE_270X200 = "landscape_xlarge", // 270x200px
+  AMAZING_250X156 = "landscape_amazing", // 250x156px
+  INCREDIBLE_464X261 = "landscape_incredible", // 464x261px
+}
