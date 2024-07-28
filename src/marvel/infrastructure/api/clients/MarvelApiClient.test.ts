afebf607320fb2a80104491fc99c06b7122690a8
Michael Coloma Calva <neomichael87@gmail.com>
2024-07-28 17:12:35 +0200

    Update to add test to MarvelApiClient

--- src/marvel/infrastructure/api/clients/MarvelApiClient.test.ts
+++ src/marvel/infrastructure/api/clients/MarvelApiClient.test.ts
@@ -28,6 +28,66 @@ const responseMockApiCharacters = {
   },
 };
 
+const responseMockApiCharacterDetails = {
+  code: 200,
+  status: "Ok",
+  data: {
+    offset: 0,
+    limit: 1,
+    total: 1,
+    count: 1,
+    results: [
+      {
+        id: 1011334,
+        name: "3-D Man",
+        description: "",
+        modified: "2014-04-29T14:18:17-0400",
+        thumbnail: {
+          path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+          extension: "jpg",
+        },
+        comics: {
+          available: 12,
+          collectionURI: "http://gateway.marvel.com/v1/public/characters/1011334/comics",
+          items: [
+            {
+              resourceURI: "http://gateway.marvel.com/v1/public/comics/21366",
+              name: "Avengers: The Initiative (2007) #14",
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+const responseMockApiCharacterDetailsComics = {
+  code: 200,
+  status: "Ok",
+  data: {
+    offset: 0,
+    limit: 20,
+    total: 20,
+    count: 20,
+    results: [
+      {
+        id: 21366,
+        title: "Avengers: The Initiative (2007) #14",
+        thumbnail: {
+          path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+          extension: "jpg",
+        },
+        dates: [
+          {
+            type: "onsaleDate",
+            date: "2021-09-29T00:00:00-0400",
+          },
+        ],
+      },
+    ],
+  },
+};
+
 describe("PodcastsApiClient", () => {
   let client: MarvelApiClient;
   let mockAxios: MockAdapter;
@@ -67,6 +127,75 @@ describe("PodcastsApiClient", () => {
       mockAxios.onGet(url).reply(500, { error: "Internal Server Error" });
       const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => "error");
       await expect(client.fetchTopCharacters()).rejects.toThrowError("Request failed with status code 500");
+      consoleErrorSpy.mockRestore();
+    });
+  });
+
+  describe("fetchCharacterDetails test", () => {
+    it("fetches character details successfully", async () => {
+      const characterId = "1011334";
+      const url = generateMarvelApiUrl({
+        path: `characters/${characterId}`,
+        params: { limit: client.LIMIT_FETCH_CHARACTER_DETAILS },
+        isTest: true,
+      });
+
+      mockAxios.onGet(url).reply(200, responseMockApiCharacterDetails);
+      const responseExpected = await client.fetchCharacterDetails(characterId);
+
+      expect(mockAxios.history.get.length).toBe(1);
+      expect(mockAxios.history.get[0].url).toBe(url);
+      expect(responseExpected).toEqual(responseMockApiCharacterDetails.data.results[0]);
+    });
+
+    it("should throw an error when fetching character details fails", async () => {
+      const characterId = "1011334";
+      const url = generateMarvelApiUrl({
+        path: `characters/${characterId}`,
+        params: { limit: client.LIMIT_FETCH_CHARACTER_DETAILS },
+        isTest: true,
+      });
+      mockAxios.onGet(url).reply(500, { error: "Internal Server Error" });
+      const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => "error");
+      await expect(client.fetchCharacterDetails(characterId)).rejects.toThrowError(
+        "Request failed with status code 500",
+      );
+      consoleErrorSpy.mockRestore();
+    });
+  });
+
+  describe("fetchCharacterDetailsComics test", () => {
+    it("fetches character details comics successfully", async () => {
+      const urlComics = "characters/1011334/comics";
+      const url = generateMarvelApiUrl({
+        path: urlComics,
+        params: { limit: client.LIMIT_FETCH_CHARACTERS_DETAILS_COMICS },
+        isTest: true,
+        withBaseURL: false,
+      });
+
+      mockAxios.onGet(url).reply(200, responseMockApiCharacterDetailsComics);
+      const responseExpected = await client.fetchCharacterDetailsComics(urlComics);
+
+      expect(mockAxios.history.get.length).toBe(1);
+      expect(mockAxios.history.get[0].url).toBe(url);
+      expect(responseExpected.length).toEqual(responseMockApiCharacterDetailsComics.data.results.length);
+      expect(responseExpected[0]).toEqual(responseMockApiCharacterDetailsComics.data.results[0]);
+    });
+
+    it("should throw an error when fetching character details comics fails", async () => {
+      const urlComics = "characters/1011334/comics";
+      const url = generateMarvelApiUrl({
+        path: urlComics,
+        params: { limit: client.LIMIT_FETCH_CHARACTERS_DETAILS_COMICS },
+        isTest: true,
+        withBaseURL: false,
+      });
+      mockAxios.onGet(url).reply(500, { error: "Internal Server Error" });
+      const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => "error");
+      await expect(client.fetchCharacterDetailsComics(urlComics)).rejects.toThrowError(
+        "Request failed with status code 500",
+      );
       consoleErrorSpy.mockRestore();
     });
   });
